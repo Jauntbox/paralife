8c2b96dd1eeb25cfe55a39b0f09860c6f47bc782
Kevin Moore <jauntbox@gmail.com>
2015-06-05 00:41:36 -0700

    Added new decomposition technique so each process only stores and computes on local data, with ghost cells communicated between processes afterwards. Seems to work, but tricky to check since visualization is harder this way. I'll work on a visualization test for the next version

--- paralife.py
+++ paralife.py
@@ -19,24 +19,105 @@ class Life:
 	'''N: length & width of the simulation in number of cells'''
 	def __init__(self, N):
 		self.N = N
+		self.comm = MPI.COMM_WORLD
+		self.mpi_size = comm.Get_size()
+		self.rank = comm.Get_rank()
 		self.grid = np.random.choice(2, (N,N))
 		self.aug_grid = self.form_aug_grid(self.grid)
+
+		#If we're doing the point-to-point communication method, then each process
+		#only needs to hold a small part of the entire simulation. The first and
+		#last rows/columns of self.proc_grid will be 'ghost' rows/columns for the 
+		#periodic boundaries. The actual rows we care about will range from indices
+		#1-N (indices 0 and N+1 are ghost cells). Likewise, the columns we will care
+		#about have indices 1-self.cols_per_proc with the first & last being ghosts.
+		if(self.mpi_size > 1):
+			recv_buf = np.arange(N+2)
+			self.cols_per_proc = int(np.ceil(N*1.0/self.mpi_size))
+			self.proc_grid = np.random.choice(2, (N+2,self.cols_per_proc+2))
+
+			#Constant initialized grid for testing with N=4 and 2 procs:
+			#if(self.rank==0):
+			#	self.proc_grid = np.array([[0,1,1,0],[1,0,1,0],[0,1,0,0],[1,0,0,0],[1,1,0,0],[0,1,0,1]])
+			#if(self.rank==1):
+			#	self.proc_grid = np.array([[1,0,0,1],[0,1,0,1],[1,0,1,1],[0,1,1,1],[0,0,1,1],[1,0,1,0]])
+
+			#Still life for testing with N=4 and 2 procs:
+			#if(self.rank==0):
+			#	self.proc_grid = np.array([[0,0,0,0],[1,1,1,1],[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]])
+			#if(self.rank==1):
+			#	self.proc_grid = np.array([[0,0,0,0],[1,1,1,1],[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]])
+
+			#Make the bottom/top boundaries reflective:
+			self.proc_grid[0,:] = self.proc_grid[N,:]
+			self.proc_grid[N+1,:] = self.proc_grid[1,:]
+
+			#Set up all the ghost cells between the processes:
+			self.comm_ghosts()
+
 		self.fig = plt.figure()
+
+	'''Communicates the ghost cells between the processes'''
+	def comm_ghosts(self):
+		if(self.mpi_size > 1):
+			recv_buf_ary = np.arange(N+2)	#Buffer for receiving an array
+			recv_buf_num = np.arange(1)		#Buffer for receiving a number
+			#Integer tags for where data was *sent* from (so as to ensure it arrives in the correct buffer):
+			[left_tag, right_tag, corner_tl_tag, corner_bl_tag, corner_tr_tag, corner_br_tag] = range(6)
+
+			#Send ghost cells & columns from the neighboring processes:
+			comm.Isend(np.array(self.proc_grid[:,1]), dest=(self.rank-1)%self.mpi_size, tag=left_tag)
+			comm.Isend(np.array(self.proc_grid[:,self.cols_per_proc]),dest=(self.rank+1)%self.mpi_size, tag=right_tag)
+			#If we're at the edges, then also send the corners:
+			#if(self.rank==0):
+			#	comm.Isend(self.proc_grid[1,1], dest=self.mpi_size-1, tag=corner_tl_tag)
+			#	comm.Isend(self.proc_grid[N,1], dest=self.mpi_size-1, tag=corner_bl_tag)
+			#if(self.rank==self.mpi_size-1):
+			#	comm.Isend(self.proc_grid[1,self.cols_per_proc], dest=0, tag=corner_tr_tag)
+			#	comm.Isend(self.proc_grid[N,self.cols_per_proc], dest=0, tag=corner_br_tag)
+
+			#Receive ghost cells & columns from the neighboring processes:
+			comm.Recv(recv_buf_ary, source=(self.rank-1)%self.mpi_size, tag=right_tag)
+			self.proc_grid[:,0] = recv_buf_ary
+			comm.Recv(recv_buf_ary, source=(self.rank+1)%self.mpi_size, tag=left_tag)
+			self.proc_grid[:,self.cols_per_proc+1] = recv_buf_ary
+			#if(self.rank==0):
+			#	comm.Recv(recv_buf_num, source=self.mpi_size-1, tag=corner_tr_tag)
+			#	self.proc_grid[N+1,0] = recv_buf_num
+			#	comm.Recv(recv_buf_num, source=self.mpi_size-1, tag=corner_br_tag)
+			#	self.proc_grid[0,0] = recv_buf_num
+			#if(self.rank==self.mpi_size-1):
+			#	comm.Recv(recv_buf_num, source=0, tag=corner_tl_tag)
+			#	self.proc_grid[N+1,self.cols_per_proc+1] = recv_buf_num
+			#	comm.Recv(recv_buf_num, source=0, tag=corner_bl_tag)
+			#	self.proc_grid[0,self.cols_per_proc+1] = recv_buf_num
+
 
 	def __str__(self):
 		return str(self.grid)
+
 
 	'''Forms an augmented grid so that calculations can be done entirely in place:'''
 	def form_aug_grid(self,a):
 		return np.lib.pad(a, ((1,1),(1,1)), 'wrap')
 
-	'''Function that sums all the neighbors of the (row,col)'th entry of the grid'''
+
+	'''Function that sums all the neighbors of the (row,col)'th entry of the grid using
+		data from the global simulation'''
 	def neighbor_sum(self,row,col):
 		#aug_grid[row+1][col+1] is the element we're interested in in the augmented grid.
 		#It's guaranteed to have all 8 neighbors defined by constructrion.
 		return self.aug_grid[row+2][col+2] + self.aug_grid[row+2][col+1] + self.aug_grid[row+2][col] + \
 			self.aug_grid[row+1][col+2] + self.aug_grid[row+1][col] + \
 			self.aug_grid[row][col+2] + self.aug_grid[row][col+1] + self.aug_grid[row][col]
+
+	'''Function that sums all the neighbors of the (row,col)'th entry of the grid using
+		data local to this process (self.proc_grid)'''
+	def neighbor_sum_local(self,row,col):
+		return self.proc_grid[row+1][col+1] + self.proc_grid[row+1][col] + self.proc_grid[row+1][col-1] + \
+			self.proc_grid[row][col+1] + self.proc_grid[row][col-1] + \
+			self.proc_grid[row-1][col+1] + self.proc_grid[row-1][col] + self.proc_grid[row-1][col-1]
+
 
 	'''Function that updates the grid according to the rules of the game of life using
 		a toroidal geometry (wrapping in both directions - like asteroids)'''
@@ -50,6 +131,30 @@ class Life:
 		
 		plt.cla()	#Clear the plot so things draw much faster
 		return plt.imshow(self.grid, interpolation='nearest')
+
+
+	'''Updates the matrix by splitting things into columns and then using point to
+		point communication to share boundary columns between processes. Each 
+		process now gets a contiguous set of columns to operate on, so the number
+		of processes should not be greater than the number of columns (otherwise
+		several of them will not be used).'''
+	def update_para_1d_dec_point_to_point(self,i):
+		#Make a temporary grid so that we don't overwrite cells while working on
+		#the update step:
+		temp_grid = self.proc_grid
+
+		for row in range(1,N+1):
+			for col in range(1,self.cols_per_proc+1):
+				if self.neighbor_sum_local(row,col) == 3:
+					temp_grid[row][col] = 1
+				elif self.neighbor_sum_local(row,col) != 2:
+					temp_grid[row][col] = 0
+		self.proc_grid = temp_grid
+
+		#Now send the updated columns to the necessary
+		comm.barrier()
+		self.comm_ghosts()
+
 
 	'''Updates the simulation using a 1d domain decomposition: every process works
 		on a group of rows determined by that processor's rank, then the results are
@@ -83,6 +188,7 @@ class Life:
 		plt.cla()	#Clear the plot so things draw much faster
 		return plt.imshow(self.grid, interpolation='nearest')
 
+
 	'''Updates the simulation using a 2d domain decomposition: each process gets 
 		a rectangular (square?) patch to work on '''
 	def update_para_2d_dec(self,i):
@@ -113,6 +219,7 @@ class Life:
 		plt.cla()	#Clear the plot so things draw much faster
 		return plt.imshow(self.grid, interpolation='nearest')
 
+
 	'''Display one step of the simulation'''
 	def show(self):
 		#fig, ax = plt.subplots()
@@ -121,12 +228,14 @@ class Life:
 		#im = ax.imshow(self.grid, interpolation='nearest')
 		plt.show()
 
+
 	'''Play a movie of the simulation'''
 	def movie(self):
 		#fig, ax = plt.subplots()
 		#mat = ax.matshow(self.grid)
 		anim = animation.FuncAnimation(self.fig, self.update, interval=10)
 		plt.show()
+
 
 	'''Play a movie of the simulation with parallelization.'''
 	def movie_para(self):
@@ -135,7 +244,7 @@ class Life:
 		anim = animation.FuncAnimation(self.fig, self.update_para_1d_dec, interval=10)
 		plt.show()
 
-N = 50 #Size of grid for the game of life
+N = 40 #Size of grid for the game of life
 comm = MPI.COMM_WORLD
 mpi_size = comm.Get_size()
 #Figure out MPI division of tasks (how many rows per processor)
@@ -152,15 +261,17 @@ else:
 	print "Running in serial mode."
 
 
-do_movie = True
+do_movie = False
 game = Life(N) #Initialize the game with a grid of size N
 
 #Make proc 0 have the true matrix - send it to the others:
 game.grid = comm.bcast(game.grid, root=0)
 game.aug_grid = game.form_aug_grid(game.grid)
 rank = comm.Get_rank()
-print rank, game
-print ""
+#print rank, game
+#print ""
+if(game.mpi_size > 1):
+	print rank, game.proc_grid
 
 #Running with the movie enabled in parallel will cause an animation to pop up
 #for each process. This is pretty impractical and only intended for testing
@@ -175,7 +286,9 @@ if(do_movie):
 else:
 	num_steps = 100
 	for i in range(num_steps):
-		game.update_para_1d_dec(i)
+		game.update_para_1d_dec_point_to_point(i)
+		print rank, game.proc_grid
+		#game.update_para_1d_dec(i)
 
 
 #Want parallel version to look like:
